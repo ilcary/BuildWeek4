487377eff1e936b931ce0abc21c2cb0098c20688
ChiaraSoddu <106075562+ChiaraSoddu@users.noreply.github.com>
2022-10-18 17:19:08 +0200

    Add files via upload

---
+++ BuildWeek4/src/main/java/mezzi/Tram.java
@@ -0,0 +1,30 @@
+package mezzi;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Tram extends Mezzi{
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	
+	public Tram() {
+		
+	}
+
+	public Tram(int id, Stato stato, String inMain, String inServ, int seats, int route_id ) {
+		super();
+		this.id= id;
+		super.stato= stato;
+		super.inMain= inMain;
+		super.inServ= inServ;
+		super.seats= seats;
+		super.route_id= route_id;
+		
+	}
+	
+}
